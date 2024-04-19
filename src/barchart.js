f155fd807da75d7c0fc17747f79cc7367ffea2cf
varanasisainarayana <varanasisairock@gmail.com>
2024-04-19 13:24:46 +0530

    Added figur in bar graph

--- src/barchart.js
+++ src/barchart.js
@@ -25,35 +25,73 @@ function BarChart() {
 
 
   const data = {
-    labels: ['Retirement Corpis', 'Existing Investments', 'Corpus Balence'],
-    datasets: [
-      {
-        label: '# of Votes',
-        data: [rc, second, third],
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-        ],
-        borderWidth: 1,
-      },
-    ],
-  };
-
-  const options = {
-    scales: {
-      y: {
-        beginAtZero: true,
-      },
+  labels: ['Retirement Corpis', 'Existing Investments', 'Corpus Balence'],
+  datasets: [
+    {
+      label: '# of Votes',
+      data: [rc, second, third],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+      ],
+      borderWidth: 1,
     },
-  };
+  ],
+};
 
-  return <Bar data={data} options={options} />;
+const options = {
+  plugins: {
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart - Stacked'
+    },
+    tooltip: {
+      mode: 'index',
+      intersect: false
+    },
+  },
+  responsive: true,
+  scales: {
+    x: {
+      stacked: true,
+    },
+    y: {
+      beginAtZero: true,
+      stacked: true
+    }
+  },
+  animation: {
+  onComplete: (animation) => {
+    var chartInstance = animation.chart,
+    ctx = chartInstance.ctx;
+    ctx.textAlign = 'center';
+    ctx.textBaseline = 'bottom';
+    ctx.font = 'bold 14px Arial'; // font style and size
+
+    var colors = [ 'rgba(255, 99, 132, 0.7)',
+        'rgba(54, 162, 235, 0.7)',
+        'rgba(255, 206, 86, 0.7)',
+    ]; // array of colors
+
+    chartInstance.data.datasets.forEach((dataset, i) => {
+      var meta = chartInstance.getDatasetMeta(i);
+      meta.data.forEach((bar, index) => {
+        var data = dataset.data[index];
+        ctx.fillStyle = colors[index % colors.length]; // set the fillStyle to the color at the current index
+        ctx.fillText(data, bar.x, bar.y - 5);
+      });
+    });
+  }
+}
+};
+
+return <Bar data={data} options={options} />;
 }
 
 export default BarChart;
